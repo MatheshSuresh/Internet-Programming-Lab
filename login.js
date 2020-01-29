56128f658e40f4efc0ff502a35746a5a865b4049
Mathesh Suresh <33441626+MatheshSuresh@users.noreply.github.com>
2020-01-29 02:52:12 -0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,26 @@
+var attempt = 3; 
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "un" && password == "pw"){
+alert ("Login successfully");
+window.location = "success.html";
+
+
+
+
+
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
